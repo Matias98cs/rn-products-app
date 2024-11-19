f2dbcb0964f40bef30f783336a936bb7b51207b9
Matias Campos <matias98cs@gmail.com>
2024-11-19 19:03:55 -0300

    agregando fuentes de google al proyecto

--- app/(products-app)/(home)/index.tsx
+++ app/(products-app)/(home)/index.tsx
@@ -1,13 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { ThemedText } from '@/presentation/theme/components/ThemedText'
+import { View, Text } from "react-native";
+import React from "react";
+import { ThemedText } from "@/presentation/theme/components/ThemedText";
+import { useThemeColor } from "@/presentation/theme/hooks/useThemeColor";
 
 const HomeScreen = () => {
-  return (
-    <View>
-      <ThemedText>HomeScreen</ThemedText>
-    </View>
-  )
-}
 
-export default HomeScreen
+  const primary = useThemeColor({}, 'primary');
+
+  return (
+    <View
+      style={{
+        paddingTop: 100,
+        paddingHorizontal: 20,
+      }}
+    >
+      <ThemedText
+        style={{
+          fontFamily: "KanitBold",
+          color: primary
+        }}
+      >
+        HomeScreen
+      </ThemedText>
+
+      <ThemedText
+        style={{
+          fontFamily: "KanitRegular",
+        }}
+      >
+        HomeScreen
+      </ThemedText>
+
+      <ThemedText
+        style={{
+          fontFamily: "KanitThin",
+        }}
+      >
+        HomeScreen
+      </ThemedText>
+
+      <ThemedText>
+        HomeScreen
+      </ThemedText>
+    </View>
+  );
+};
+
+export default HomeScreen;
