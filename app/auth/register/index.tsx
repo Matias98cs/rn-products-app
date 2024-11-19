1e0cbb509c95f4f4caaad089ba7aa3362be1a3ad
Matias Campos <matias98cs@gmail.com>
2024-11-19 18:53:47 -0300

    products app - iniciando

---
+++ app/auth/register/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { ThemedView } from '@/presentation/theme/components/ThemedView'
+
+const RegisterScreen = () => {
+  return (
+    <View>
+      <ThemedView>RegisterScreen</ThemedView>
+    </View>
+  )
+}
+
+export default RegisterScreen
