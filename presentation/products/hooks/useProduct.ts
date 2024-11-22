85e12f3d9754e03f04b25eef10ebc6809397f96f
Matias Campos <matias98cs@gmail.com>
2024-11-22 20:17:53 -0300

    limpiar cache para mostar el producto actualizado

--- presentation/products/hooks/useProduct.ts
+++ presentation/products/hooks/useProduct.ts
@@ -1,11 +1,12 @@
 import { updateCreateProduct } from "@/core/produdcts/actions/create-update-product.actions";
 import { getProductById } from "@/core/produdcts/actions/get-product-by-id.actions"
 import { Product } from "@/core/produdcts/interfaces/producto.interface";
-import { useMutation, useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { Alert } from "react-native";
 
 const useProduct = (productId: string) => {
 
+    const queryClient = useQueryClient()
     const productQuery = useQuery({
         queryKey: ["product", productId],
         queryFn: () => getProductById(productId),
@@ -16,6 +17,13 @@ const useProduct = (productId: string) => {
     const productMutation = useMutation({
         mutationFn: async (data: Product) => updateCreateProduct(data),
         onSuccess: (data: Product) => {
+            queryClient.invalidateQueries({
+                queryKey: ["products", "infinite"]
+            });
+
+            queryClient.invalidateQueries({
+                queryKey: ["product", data.id]
+            });
             Alert.alert("Producto guaradado", "El producto se guardo correctamente")
         }
     })
