f07e5b0ad20baa0c7f6f814f5f4e0a5a80de80b2
Matias Campos <matias98cs@gmail.com>
2024-11-21 22:36:46 -0300

    infinite srcoll y pull to refresh

--- presentation/products/components/ProductList.tsx
+++ presentation/products/components/ProductList.tsx
@@ -1,6 +1,9 @@
 import { Product } from "@/core/produdcts/interfaces/producto.interface";
-import { View, Text, FlatList } from "react-native";
 import { ProductCard } from "./ProductCard";
+import { useState } from "react";
+import { FlatList } from "react-native-gesture-handler";
+import { useQueryClient } from "@tanstack/react-query";
+import { RefreshControl } from "react-native";
 
 interface Props {
   products: Product[];
@@ -8,6 +11,21 @@ interface Props {
 }
 
 const ProductList = ({ products, loadNextPage }: Props) => {
+  const queryClient = useQueryClient();
+
+  const [isRefresing, setIsRefresing] = useState(false);
+
+  const onPullToRefresh = async () => {
+    console.log('llamando')
+    setIsRefresing(true);
+    await new Promise((resolve, reject) => setTimeout(resolve, 200));
+
+    queryClient.invalidateQueries({
+      queryKey: ["products", "infinite"],
+    });
+
+    setIsRefresing(false);
+  };
   return (
     <FlatList
       data={products}
@@ -15,6 +33,11 @@ const ProductList = ({ products, loadNextPage }: Props) => {
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => <ProductCard product={item} />}
       showsVerticalScrollIndicator={false}
+      onEndReached={loadNextPage}
+      onEndReachedThreshold={0.8}
+      refreshControl={
+        <RefreshControl refreshing={isRefresing} onRefresh={onPullToRefresh} />
+      }
     />
   );
 };
